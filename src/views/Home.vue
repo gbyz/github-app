0e7565b6642ebc87bff7069e7b73e34c2fa89972
Can Yılmaz <canylmz6134@gmail.com>
2018-07-22 19:03:37 +0300

    Home @view UPDATED | user search Completed v 0.1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,50 @@
 <template>
   <div class="home">
-   <Search/>
+      <Search v-on:SearchRequested="handleSearch"></Search>
+      <div v-for="(value,key) in users" >
+          <UserProfile  :name="value.login" :key="key"/>
+      </div>
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Search from '../components/Search'
+import UserProfile from '../components/UserProfile'
+import HTTP from '../service'
 
-export default {
+export default { 
   name: 'home',
   components: {
+      UserProfile,
     Search
-  }
+  },
+    data(){
+    return {
+        users:[]
+    }
+    },
+    methods:{
+      getUserSearch(query){
+          HTTP.get('search/users',{
+              params:{
+                  q:query
+              }
+          }).then((res)=>{
+                  this.users=res.data.items;
+              console.log(res.data.items)
+              })
+      },
+        handleSearch (query) {
+            this.users = [];
+            this.getUserSearch(query)
+        }
+    },
+    created(){
+      this.getUserSearch('gelistirirken')
+    }
+
 }
 </script>
