d7175ca16ab2ab72023d0bd1f62fe6e0bf6980c6
Can Yılmaz <canylmz6134@gmail.com>
2018-07-22 19:01:51 +0300

    router UPDATED

--- src/router.js
+++ src/router.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import About from './views/About.vue'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -12,16 +11,7 @@ export default new Router({
       name: 'home',
       component: Home
     },
-    {
-      path: '/user',
-      name: 'user',
-      component:User
-    },
-    {
-      path:'/repositories',
-      name: 'repositories',
-      components: Repositories
-    },
+
     
   ],
   mode: 'history'
