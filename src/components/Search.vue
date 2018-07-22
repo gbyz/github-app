55157af172323c9f2fce21646e11196fe60077af
Can Yılmaz <canylmz6134@gmail.com>
2018-07-22 19:02:27 +0300

    Search @component UPDATED UserProfile @component CREATED

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,14 +1,36 @@
 <template>
-    <div id="search">
-        <input type="text"/>
-        <button >Search</button>
+    <div class="search">
+        <input type="text" v-model="query" @keypress.enter="handleSearch" />
+        <button @click="handleSearch">Search</button>
     </div>
 </template>
-<script>
-export default {
-    
-}
-</script>
-<style>
 
+<script>
+    export default {
+        data: function () {
+            return {
+                query: ''
+            }
+        },
+        methods: {
+            handleSearch () {
+                this.$emit('SearchRequested', this.query)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .search input{
+        padding: 10px 20px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        width: 400px;
+    }
+    .search button{
+        padding: 5px 15px;
+        border:1px solid #ccc;
+        background-color: #c1c;
+        color: #fff;
+    }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p>{{name}}</p>
+        <hr/>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "UserProfile",
+        props:{
+            name: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
