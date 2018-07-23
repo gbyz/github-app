bc4b893e9f98360d7101db335b3a5137f5c2bd25
Can Yılmaz <canylmz6134@gmail.com>
2018-07-23 11:01:16 +0300

    store UPDATED | fetchUsers, fetchUser, fetsUserRepos actions ADDED

--- src/store.js
+++ src/store.js
@@ -1,16 +1,58 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import HTTP from './service'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+      users:{},
+      user:{},
+      repos:{},
+      repo:{},
 
   },
   mutations: {
-
+      setUser(state, user) {
+          state.user = user
+      },
+      setUsers(state, users) {
+          state.users = users;
+      },
+      setRepos(state, repos) {
+          state.repos = repos
+      },
+      setRepo(state, repo) {
+          state.repo = repo;
+      },
   },
   actions: {
+      fetchUsers({commit},query){
+          return HTTP.get('search/users',{
+              params:{
+                  q:query
+              }
+          }).then((res)=>{
+              commit('setUsers', res.data.items);
+          })
+      },
+      fetchUser({commit},query){
+          return HTTP.get('search/users',{
+              params:{
+                  q:query
+              }
+          }).then((res)=>{
+              commit('setUser', res.data.items[0]);
+          })
+      },
+      fetchUserRepos({commit,dispatch},query) {
+          HTTP.get(`users/${query}/repos`)
+              .then((res) => {
+                  commit('setRepos', res.data);
+                  dispatch('fetchUser', query);
+              })
+
+      },
 
   }
 })
