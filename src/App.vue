5befd30672b2f4bc6eba08490bc402cb465673d0
Can Yılmaz <canylmz6134@gmail.com>
2018-07-23 10:47:01 +0300

    App UPDATED | routes CHANGED

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Anasayfa</router-link> |
-      <router-link to="/about">About</router-link>
+      <!--<router-link to="/about">About</router-link>-->
     </div>
     <router-view/>
   </div>
