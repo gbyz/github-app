bfb1667975da69009087431f7120b1b75177d404
Beys <sumeyyebeyzasahin@gmail.com>
2018-07-25 12:40:13 +0300

    Logo CHANGED/ App UPDATED/ Repo @c repo object UPDATED

--- src/components/Repo.vue
+++ src/components/Repo.vue
@@ -3,27 +3,20 @@
             <div class="col-8 pr-3">
                 <h4>
                     <a class="v-align-middle" href="">{{repo.full_name}}<em></em></a>
-
                 </h4>
-
                 <p class="col-9 d-inline-block text-gray mb-2 pr-4">
                    {{repo.description}}
                 </p>
-
                 <div class="d-flex">
-
                     <p class="f6 text-gray mr-3 mb-0 mt-2">
                         Updated <span datetime="2018-04-02T12:11:25Z" title="2 Nis 2018 15:11 GMT+3">on 2 Apr</span>
                     </p>
-
                 </div>
             </div>
-
             <div class="d-table-cell col-2 text-gray pt-2">
                 <span class="repo-language-color ml-0" style="background-color:#f1e05a;"></span>
                 {{repo.language}}
             </div>
-
             <div class="col-2 text-right pt-1 pr-3 pt-2">
                 <a class="muted-link" >
                     <svg aria-label="star" class="octicon octicon-star" viewBox="0 0 14 16" version="1.1" width="14" height="16" role="img"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"></path></svg>
@@ -37,7 +30,12 @@
     export default {
         name: "Repo",
         props:{
-            repo:{}
+            
+            repo:{
+                type:Object,
+                required:true,
+                
+            }
         }
     }
 </script>
